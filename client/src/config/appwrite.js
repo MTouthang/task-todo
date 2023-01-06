971bbd570f764ef57f2dba3a7fae90eb46f975cf
Mamang Touthang <huebart16@gmail.com>
2023-01-06 22:45:12 +0530

    appwrite endpoint change

--- client/src/config/appwrite.js
+++ client/src/config/appwrite.js
@@ -1,7 +1,7 @@
 import { Client, Account } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("http://115.99.183.81/v1") // Your API Endpoint
+  .setEndpoint("http://localhost/v1") // Your API Endpoint
   .setProject("6388a609c191b6091f86"); // Your project ID
 
 const account = new Account(client);
