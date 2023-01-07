d54012352153dc8609e223c73dd9a31c51fd3cf1
Mamang Touthang <huebart16@gmail.com>
2023-01-07 17:32:42 +0530

    appwrite via gitpot

--- client/src/config/appwrite.js
+++ client/src/config/appwrite.js
@@ -1,8 +1,12 @@
 import { Client, Account } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("http://localhost/v1") // Your API Endpoint
-  .setProject("6388a609c191b6091f86"); // Your project ID
+  .setEndpoint(
+    "https://8080-appwrite-integrationfor-3zvuaeini87.ws-us81.gitpod.io/v1"
+  ) // Your API Endpoint
+
+  // 6388a609c191b6091f86
+  .setProject("63b95e14bd5df27efa0f"); // Your project ID
 
 const account = new Account(client);
 
